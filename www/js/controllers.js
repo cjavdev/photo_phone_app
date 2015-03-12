a641c11f65998416c8c84eb7607627e50b230812
CJ Avilla <cjavilla@gmail.com>
2015-03-11 18:35:36 -0700

    a little dash of thinngs

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,49 +1,52 @@
 angular.module('starter.controllers', [])
-.controller('DashCtrl', function($scope) {})
-.controller('LoginCtrl', function($scope, $location, User) {
-  function checkLogin () {
-    if(User.loggedIn()) {
-      $location.path("/");
+  .controller('DashCtrl', function ($scope) {})
+  .controller('LoginCtrl', function ($scope, $location, User) {
+    function checkLogin() {
+      if (User.loggedIn()) {
+        $location.path("/");
+      }
     }
-  }
-  checkLogin();
+    checkLogin();
 
-  $scope.user = {};
-  $scope.login = function () {
-    User.login($scope.user).then(checkLogin);
-  };
-})
-.controller('ShootCtrl', function($scope, Camera, Description) {
-  $scope.description = {};
+    $scope.user = {};
+    $scope.login = function () {
+      User.login($scope.user).then(checkLogin);
+    };
+  })
+  .controller('ShootCtrl', function ($scope, Camera, Description) {
+    $scope.description = {};
 
-  function next() {
-    Description.nextPhotoable().then(function (description) {
-      $scope.description = description;
-    });
-  }
-  next();
-
-  $scope.nextDescription = function () {
+    function next() {
+      Description.nextPhotoable().then(function (description) {
+        $scope.description = description;
+      });
+    }
     next();
-  };
 
-  $scope.getPhoto = function() {
-    Camera.getPicture().then(function(imageURI) {
-      console.log(imageURI);
-    }, function(err) {
-      console.err(err);
+    $scope.nextDescription = function () {
+      next();
+    };
+
+    $scope.getPhoto = function () {
+      Camera.getPicture().then(function (imageURI) {
+        console.log(imageURI);
+      }, function (err) {
+        console.err(err);
+      });
+    };
+  })
+  .controller('DescribeCtrl', function ($scope, $location, Photo) {
+    $scope.description = {};
+
+    Photo.nextDescribable().then(function (photo) {
+      $scope.photo = photo;
+      $scope.description.photo_id = photo.id;
     });
-  };
-})
-.controller('DescribeCtrl', function($scope, Photo) {
-  $scope.description = {};
 
-  Photo.nextDescribable().then(function (photo) {
-    $scope.photo = photo;
-    $scope.description.photo_id = photo.id;
+    $scope.saveDescription = function () {
+      Photo.describe($scope.description)
+        .then(function () {
+          $location.path("/tab/shoot");
+        });
+    };
   });
-
-  $scope.saveDescription = function () {
-    Photo.describe($scope.description);
-  };
-});
